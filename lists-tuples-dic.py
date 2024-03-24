eed2bdcd3fb17b32acbb0c0e642647bbbe5bd62f
cmurilosan <cmurilo.san@gmail.com>
2024-03-24 15:20:43 -0300

    Labs 1 to 9

---
+++ lists-tuples-dic.py
@@ -0,0 +1,42 @@
+print('Exercise 01')
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print('------------')
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+print('------------')
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+print('------------\nExercise 02')
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+print('------------\nExercise 03')
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print('-----------')
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
